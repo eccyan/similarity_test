63cf8ac4e21f729eea212a0d3a55794751e6b054
eccyan <g00.eccyan@gmail.com>
2016-04-10 01:15:55 +0900

    Removes pry

--- document.rb
+++ document.rb
@@ -9,7 +9,6 @@ documents_root = Pathname('documents')
 
 wikitexts = if ARGV.empty?
   file = File.open documents_root.join('uncyc.ja.xml')
-  binding.pry
   Oga.parse_xml(file).xpath('mediawiki/page/revision/text[text()]').map(&:text)
 else
   [File.open(documents_root.join(ARGV.first)).read]
